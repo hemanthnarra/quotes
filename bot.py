03610b738e633696df1c258d946981dd620fb888
hemanthnarra <hemanthkumarsuma799@gmail.com>
2021-08-17 17:52:48 +0530

    bot.py added

--- bot.py
+++ bot.py
@@ -1,10 +1,24 @@
-import telegram
+from telegram import update
+from telegram.ext import Updater, dispatcher, CommandHandler
+import requests
 
+def get_quote():
+    contents = requests.get('https://free-quotes-api.herokuapp.com/').json()
+    required = contents['quote']
+    return required
 
-bot = telegram.Bot(token='1427700541:AAFZ_Nj3TYlGbU0lMezoiNA24jWOR65_-0E')
-# print(bot.getMe())
+def quote(update, context):
+    required = get_quote()
+    chat_id = '-1001496045934'
+    context.bot.send_message(chat_id=chat_id,text=required)
 
-updates = bot.getUpdates()
-print(updates[1])
+def main():
+    bot_token = os.environ.get('BOT_TOKEN','')
+    updater = Updater(bot_token)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('quote', quote))
+    updater.start_polling()
+    updater.idle()
 
-bot.sendMessage(text='hello,hemanth', chat_id=705433594)
+if __name__ == '__main__':
+    main()
